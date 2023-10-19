e145c9c3144a66a9a3cc0be9b5559dcde744cb76
v <v@localhost>
2023-10-19 07:02:13 -0700

    initial marshal and helper scripts

---
+++ sops/sops-helpers/is-sops-encrypted.sh
@@ -0,0 +1,53 @@
+#!/usr/bin/env bash
+
+#set -x
+
+declare ENCRYPTION_DETERMINATE2 ENCRYPTION_DETERMINATE2
+ENCRYPTION_DETERMINATE1=' *.+: "?ENC\[.*\]"?,?$' # regex
+ENCRYPTION_DETERMINATE2='^( |\t)*"?sops"?: ?\{?$' # regex
+
+declare -i rc
+
+declare -i unencrypted_count
+unencrypted_count=0
+
+declare gr
+if which 'rg' &> /dev/null; then
+  gr='rg'
+else
+  gr='grep'
+fi
+
+#############
+# FUNCTIONS #
+#############
+
+function is-file-encrypted {
+  local FILE
+  FILE="${1}"
+  if "${gr}" -q "${ENCRYPTION_DETERMINATE1}" "${FILE}" \
+    && "${gr}" -q "${ENCRYPTION_DETERMINATE2}" "${FILE}"; then
+      : # pass
+  else
+    unencrypted_count=$(( unencrypted_count + 1 ))
+  fi
+}
+
+########
+# MAIN #
+########
+
+for i in "${@}"; do
+  is-file-encrypted "${i}"
+done
+
+if [[ ${unencrypted_count} -eq 0 ]]; then
+  # if 
+  rc=0
+else
+  rc=${unencrypted_count}
+fi
+
+# the exit code is the number of files evaluated which were NOT encypted
+exit ${rc}
+
---
+++ sops/sops-marshal.sh
@@ -0,0 +1,97 @@
+#!/usr/bin/env bash
+
+###########
+# OPTARGS #
+###########
+
+declare SOPS_LIST_FILE
+
+while getopts ":f:" opt; do
+  case ${opt} in
+    f )
+      SOPS_LIST_FILE="${OPTARG}"
+      ;;
+    \?|* )
+      echo "Invalid option -${OPTARG}" 1>&2
+      ;;
+  esac
+done
+
+shift $((OPTIND -1))
+
+SOPS_LIST_FILE="${SOPS_LIST_FILE-.simple-sops-managed-files}"
+
+########
+# ARGS #
+########
+
+declare COMMAND
+case "${1}" in
+  encrypt)
+    COMMAND="encrypt-file"
+    ;;
+  decrypt)
+    COMMAND="decrypt-file"
+    ;;
+  *)
+    echo "ERROR: Only accepts 'encrypt' or 'decrypt' as an argument" >&2
+    exit 3
+    ;;
+esac
+
+
+########
+# VARS #
+########
+
+declare -a FILES_TO_MARSHAL
+mapfile -t FILES_TO_MARSHAL < "${SOPS_LIST_FILE}"
+
+#declare ENCRYPTION_DETERMINATE
+#ENCRYPTION_DETERMINATE=' *.+: "?ENC\[.*\]"?,?$' # regex
+
+# Capture the path of `is-sops-encrypted.sh`, relative to the script location
+# and not the current working directory. (Note: symlinks not accounted for)
+declare IS_SOPS_ENCRYPTED
+IS_SOPS_ENCRYPTED=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )/sops-helpers/is-sops-encrypted.sh
+
+#############
+# FUNCTIONS #
+#############
+
+function encrypt-file {
+  local FILE
+  FILE="${1}"
+  if "${IS_SOPS_ENCRYPTED}" "${FILE}"; then
+    echo "${FILE}": encrypted, skipping
+  else
+    echo -n "${FILE}": encrypting ...
+    set -e
+    sops --encrypt --in-place "${FILE}"
+    set +e
+    echo ' done'
+  fi
+}
+
+function decrypt-file {
+  local FILE
+  FILE="${1}"
+  if "${IS_SOPS_ENCRYPTED}" "${FILE}"; then
+    echo -n "${FILE}": decrypting ...
+    set -e
+    sops --decrypt --in-place "${FILE}"
+    set +e
+    echo ' done'
+  else
+    echo "${FILE}": not encrypted, skipping
+  fi
+}
+
+########
+# MAIN #
+########
+
+for i in "${FILES_TO_MARSHAL[@]}"; do
+  "${COMMAND}" "${i}"
+done
+
