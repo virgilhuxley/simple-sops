2072631ad9a944ec60e37f26663fe81ca19e2a54
Virgil Huxley <virgilhuxley@gmail.com>
2023-10-20 06:59:58 -0700

    install-sops helper script

---
+++ simple-sops/helpers/install-sops.sh
@@ -0,0 +1,59 @@
+#!/usr/bin/env bash
+
+set -e
+
+########
+# VARS #
+########
+
+binary_type="linux.amd64"
+sops_version="v3.8.1"
+sops_checksum="d6bf07fb61972127c9e0d622523124c2d81caf9f7971fb123228961021811697"
+
+dst_dir="/usr/local/bin"
+
+##############
+# VALIDATION #
+##############
+
+if [[ -x "$(command -v wget)" ]]; then
+  dl_bin="wget -nv -O-"
+elif [[ -x "$(command -v curl)" ]]; then
+  dl_bin="curl -s -L"
+else
+  echo "Missing wget or curl: This script requires wget or curl to run. Aborting" >&2
+  exit 2
+fi
+if [[ -x "$(command -v shasum)" ]]; then
+  : # pass
+else
+  echo "Missing shasum: This script requires perl or libdigest-sha-perl to run. Aborting" >&2
+  exit 2
+fi
+
+#############
+# FUNCTIONS #
+#############
+
+function install-sops {
+  echo "Installing sops..."
+  $dl_bin github.com/getsops/sops/releases/download/${sops_version}/sops-${sops_version}.${binary_type} > /tmp/sops
+  echo "${sops_checksum}  /tmp/sops" | shasum -c -
+  mv /tmp/sops ${dst_dir}/sops
+  chmod +x ${dst_dir}/sops
+  echo "Installed ${dst_dir}/sops ${sops_version}"
+}
+
+########
+# MAIN #
+########
+
+# if sops is not present install it
+if [[ ! -x "${dst_dir}/sops" ]]; then
+  install-sops
+else
+  echo "sops is already present in ${dst_dir}/sops"
+  echo "skipping install . . ."
+  exit 0
+fi
+
