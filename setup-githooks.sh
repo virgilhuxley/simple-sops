fb0bd88de4462e75f23c0020dab5da933927aa1f
v <v@localhost>
2023-10-20 06:08:52 -0700

    added githooks setup script

---
+++ setup-githooks.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+
+set -e
+
+########
+# VARS #
+########
+HOOKFILES=( simple-sops/helpers/githooks/pre-commit-check-for-unencrypted )
+
+RELATIVE_PATH='../..'
+
+GITHOOK_DIR='.git/hooks'
+
+##############
+# VALIDATION #
+##############
+if [[ -d "${GITHOOK_DIR}" ]]; then
+  : # pass
+else
+  echo "${GITHOOK_DIR} does not exist. Aborting" >&2
+  exit 2
+fi
+
+for hook in "${HOOKFILES[@]}"; do
+  if [[ -x "${hook}" ]]; then
+    : # pass
+  else
+    echo "${hook} does not exist or is not executable. Aborting" >&2
+    exit 2
+  fi
+done
+
+#########
+# MAIN #
+#########
+for i in "${HOOKFILES[@]}"; do
+  echo "creating link: ${i} -> ${GITHOOK_DIR}/$(basename ${i})"
+  ln -sf "${RELATIVE_PATH}/${i}" "${GITHOOK_DIR}/$(basename ${i})"
+done
